360b96cffc85798f7daaae5b5c092c472ecb1e45
Aaron Galagarza <aaron.galagarzacarrillo@ucr.ac.cr>
2025-06-02 13:44:48 -0600

    Primer commit del proyecto sispro

---
+++ src/main/java/cr/ac/ucr/sispro/SisproApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.sispro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/ac/ucr/sispro/SisproApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.sispro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
