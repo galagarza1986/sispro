3801322f7fcbd49295b7ff2f3e31e870465af57a
Aaron Galagarza <aaron.galagarzacarrillo@ucr.ac.cr>
2025-06-02 13:55:46 -0600

    Se creo el endpoin de saludo

--- src/main/java/cr/ac/ucr/sispro/controller/UserController.java
+++ src/main/java/cr/ac/ucr/sispro/controller/UserController.java
@@ -1,4 +1,16 @@
 package cr.ac.ucr.sispro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sispro")
 public class UserController {
+
+    @GetMapping
+    public String saludo(){
+        return "Hola mundo";
+    }
+
 }
