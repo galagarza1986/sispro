61c7432755abe50637ae55d42b218dd80c148840
Aaron Galagarza <aaron.galagarzacarrillo@ucr.ac.cr>
2025-06-02 14:11:36 -0600

    Se agrego el endepoint de findById

--- src/main/java/cr/ac/ucr/sispro/controller/UserController.java
+++ src/main/java/cr/ac/ucr/sispro/controller/UserController.java
@@ -1,6 +1,8 @@
 package cr.ac.ucr.sispro.controller;
 
+import cr.ac.ucr.sispro.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,11 @@ public class UserController {
     @GetMapping
     public String saludo(){
         return "Hola mundo";
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Integer id){
+        return new User(1,"Lola");
     }
 
 }
